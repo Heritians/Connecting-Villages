c58164851811654d3f2f6e0892deefd75cac80a1
Vishesh <vishesht27@gmail.com>
2022-07-16 19:38:16 +0530

    Resolved basic issues with carousel

--- frontend/connecting_villages/src/imageSlider.js
+++ frontend/connecting_villages/src/imageSlider.js
@@ -4,33 +4,33 @@ import "slick-carousel/slick/slick-theme.css";
 import "./style.css"
 import React from 'react'
 
-const ImageSlider = ({images}) => {
+const ImageSlider = ({ images }) => {
 
-const settings = {
-	infinite: true,
-	dots: true,
-	slidesToShow: 1,
-	slidesToScroll: 1,
-	lazyLoad: true,
-	autoplay: true,
-autoplaySpeed: 2000,
+	const settings = {
+		infinite: true,
+		dots: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		lazyLoad: true,
+		autoplay: true,
+		autoplaySpeed: 2000,
 
-};
-return (
-	<>
-	<div className="tag">
+	};
+	return (
+		<>
+			<div className="tag">
 
-	</div>
-	<div className="imgslider">
-		<Slider {...settings}>
-		{images.map((item) => (
-			<div key={item.id}>
-			<img src={item.src} alt={item.alt} />
 			</div>
-		))}
-		</Slider>
-	</div>
+			<div className="imgslider">
+				<Slider {...settings}>
+					{images.map((item) => (
+						<div key={item.id}>
+							<img src={item.src} alt={item.alt} />
+						</div>
+					))}
+				</Slider>
+			</div>
 		</>
-)
+	)
 }
 export default ImageSlider;
