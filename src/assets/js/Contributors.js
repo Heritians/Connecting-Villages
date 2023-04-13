464f6443c92e92c97421a050635c78aa2b9b2186
Anmol Garg <anmolgarg240@gmail.com>
2023-04-13 15:07:45 +0530

    Added contributors

--- src/assets/js/Contributors.js
+++ src/assets/js/Contributors.js
@@ -1,4 +1,6 @@
 import KartikeyImg from "../images/contributors/Kartikey.jpg";
+import VaibhavImg from "../images/contributors/VaibhavShrivastavaImg.jpg";
+// VaibhavShrivastavaImg.jpg
 
 const Contributors = [
   {
@@ -12,6 +14,17 @@ const Contributors = [
     github: "https://github.com/itsksquare",
     linkedin: "https://www.linkedin.com/in/kartikeykhanna/",
   },
+  {
+    id: 1,
+    name: "Vaibhav Shrivastava",
+    title: "Web Developer",
+    image: VaibhavImg,
+    imageAlt: "Vaibhav Shrivastava Image",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    github: "https://github.com/Vaibhav-shrivastav",
+    linkedin: "https://www.linkedin.com/in/vaibhav-shrivastav-84057717b/",
+  }
 ];
 
 export default Contributors;
